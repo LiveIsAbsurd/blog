8956290fe698cb1302c58c1a0c11ae6e3fe83134
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-10-05 15:42:05 +0300

    df

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -3,7 +3,15 @@ import React from 'react';
 import styles from './article.module.sass';
 
 const Article = () => {
-  return <div className={styles.item}>Article</div>;
+  return (
+    <div>
+      <div>
+        <div>Article</div>
+        <div>5</div>
+        <div>Tag 1</div>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
